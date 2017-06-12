a4d54c4acb5fb0ee5ccce27d009683de17fac033
rinmishima26@gmail.com <rinmishima26@gmail.com>
2017-06-12 02:17:20 -0700

    Articles CRUD

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,48 @@
+class PostsController < ApplicationController
+	before_action :set_post, only: [:edit, :update, :show, :destroy]
+
+	def index
+	end
+
+	def new
+		@post=Post.new
+	end
+
+	def create
+		@post=Post.new(post_params)
+		if @post.save
+			redirect_to @post, notice: "article succesfully posted"
+		else
+			render 'new', notice: "article not posted"
+		end
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+		if @post.update(post_params)
+			redirect_to @post, notice: "article succesfully updated"
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@post.destroy
+		redirect_to posts_path
+	end
+
+	private
+
+	def post_params
+		params.require(:post).permit(:title, :content)
+	end
+
+	def set_post
+		@post=Post.find(params[:id])
+	end
+end
